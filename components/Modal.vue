bab549b52fdd033a0d84dd44a02c99e3eccf0aab
Helen23781 <cardenaselena247@gmail.com>
2025-02-04 23:06:04 -0500

    validaciones

--- components/Modal.vue
+++ components/Modal.vue
@@ -1,7 +1,7 @@
 <template>
   <div v-if="visible" class="fixed inset-0 bg-gray-800 bg-opacity-75 flex justify-center items-center z-50"
     @click.self="closeModal">
-    <div class="bg-white rounded-lg shadow-lg p-6 w-full max-w-md mx-4 relative">
+    <div class="bg-white rounded-lg shadow-lg p-6 mx-4 relative" style="width: auto; max-width: 100%;">
       <button @click="closeModal" class="absolute top-2 right-2 text-black py-1 px-2 rounded hover:bg-black-600">
         Cerrar
       </button>
--- pages/estudiante.vue
+++ pages/estudiante.vue
@@ -16,36 +16,29 @@
     </div>
 
     <!-- Tabla de estudiantes -->
-    <div class="mb-8">
+    <div class="overflow-x-auto">
       <table class="min-w-full bg-white border border-gray-200">
         <thead>
           <tr>
-            <th class="hidden md:table-cell px-4 py-2 border-b-2 border-gray-200 bg-blue-200 text-black text-center">
-              Foto</th>
+            <th class="px-4 py-2 border-b-2 border-gray-200 bg-blue-200 text-black text-center">Foto</th>
             <th class="px-4 py-2 border-b-2 border-gray-200 bg-blue-200 text-black text-center">Nombre</th>
             <th class="px-4 py-2 border-b-2 border-gray-200 bg-blue-200 text-black text-center">Apellido</th>
-            <th class="hidden md:table-cell px-4 py-2 border-b-2 border-gray-200 bg-blue-200 text-black text-center">Año
-              Académico</th>
-            <th class="hidden md:table-cell px-4 py-2 border-b-2 border-gray-200 bg-blue-200 text-black text-center">
-              Edad</th>
-            <th class="hidden md:table-cell px-4 py-2 border-b-2 border-gray-200 bg-blue-200 text-black text-center">
-              Carrera</th>
-            <th class="hidden md:table-cell px-4 py-2 border-b-2 border-gray-200 bg-blue-200 text-black text-center">
-              Facultad</th>
-            <th class="hidden md:table-cell px-4 py-2 border-b-2 border-gray-200 bg-blue-200 text-black text-center">
-              Nombre de Beca</th>
-            <th class="hidden md:table-cell px-4 py-2 border-b-2 border-gray-200 bg-blue-200 text-black text-center">
-              Número de Piso</th>
-            <th class="hidden md:table-cell px-4 py-2 border-b-2 border-gray-200 bg-blue-200 text-black text-center">
-              Nombre de Torre</th>
-            <th class="hidden md:table-cell px-4 py-2 border-b-2 border-gray-200 bg-blue-200 text-black text-center">
-              Cuarto</th>
+            <th class="px-4 py-2 border-b-2 border-gray-200 bg-blue-200 text-black text-center">CI</th>
+            <th class="px-4 py-2 border-b-2 border-gray-200 bg-blue-200 text-black text-center">Año Académico</th>
+            <th class="px-4 py-2 border-b-2 border-gray-200 bg-blue-200 text-black text-center">Edad</th>
+            <th class="px-4 py-2 border-b-2 border-gray-200 bg-blue-200 text-black text-center">Carrera</th>
+            <th class="px-4 py-2 border-b-2 border-gray-200 bg-blue-200 text-black text-center">Facultad</th>
+            <th class="px-4 py-2 border-b-2 border-gray-200 bg-blue-200 text-black text-center">Nombre de Beca</th>
+            <th class="px-4 py-2 border-b-2 border-gray-200 bg-blue-200 text-black text-center">Número de Piso</th>
+            <th class="px-4 py-2 border-b-2 border-gray-200 bg-blue-200 text-black text-center">Nombre de Torre</th>
+            <th class="px-4 py-2 border-b-2 border-gray-200 bg-blue-200 text-black text-center">Cuarto</th>
+            <th class="px-4 py-2 border-b-2 border-gray-200 bg-blue-200 text-black text-center">Género</th>
             <th class="px-4 py-2 border-b-2 border-gray-200 bg-blue-200 text-black text-center">Acciones</th>
           </tr>
         </thead>
         <tbody>
           <tr v-for="estudiante in estudiantes" :key="estudiante.id" class="hover:bg-gray-50">
-            <td class="hidden md:table-cell px-4 py-2 border-b border-gray-200 text-center">
+            <td class="px-4 py-2 border-b border-gray-200 text-center">
               <NuxtImg
                 :src="estudiante.foto ? `${config.public.backend_url}/estudiantes/foto/${estudiante.foto}` : '/images/avatar.jpg'"
                 alt="Foto del estudiante" class="w-10 h-10 rounded-full object-cover cursor-pointer"
@@ -53,25 +46,20 @@
             </td>
             <td class="px-4 py-2 border-b border-gray-200 text-center">{{ estudiante.nombre_estudiante }}</td>
             <td class="px-4 py-2 border-b border-gray-200 text-center">{{ estudiante.apellido_estudiante }}</td>
-            <td class="hidden md:table-cell px-4 py-2 border-b border-gray-200 text-center">{{ estudiante.anio_academico
-              }}</td>
-            <td class="hidden md:table-cell px-4 py-2 border-b border-gray-200 text-center">{{ estudiante.edad }}</td>
-            <td class="hidden md:table-cell px-4 py-2 border-b border-gray-200 text-center">{{ estudiante.carrera }}
+            <td class="px-4 py-2 border-b border-gray-200 text-center">{{ estudiante.ci }}</td>
+            <td class="px-4 py-2 border-b border-gray-200 text-center">{{ estudiante.anio_academico }}</td>
+            <td class="px-4 py-2 border-b border-gray-200 text-center">{{ estudiante.edad }}</td>
+            <td class="px-4 py-2 border-b border-gray-200 text-center">{{ estudiante.carrera }}</td>
+            <td class="px-4 py-2 border-b border-gray-200 text-center">{{ estudiante.facultad }}</td>
+            <td class="px-4 py-2 border-b border-gray-200 text-center">{{
+              estudiante.cuarto?.torre?.piso?.beca?.nombre_beca || 'N/A' }}</td>
+            <td class="px-4 py-2 border-b border-gray-200 text-center">{{ estudiante.cuarto?.torre?.piso?.numero_piso ||
+              'N/A' }}</td>
+            <td class="px-4 py-2 border-b border-gray-200 text-center">{{ estudiante.cuarto?.torre?.nombre_torre ||
+              'N/A' }}</td>
+            <td class="px-4 py-2 border-b border-gray-200 text-center">{{ estudiante.cuarto?.numero_cuarto || 'N/A' }}
             </td>
-            <td class="hidden md:table-cell px-4 py-2 border-b border-gray-200 text-center">{{ estudiante.facultad }}
-            </td>
-            <td class="hidden md:table-cell px-4 py-2 border-b border-gray-200 text-center">
-              {{ estudiante.cuarto?.torre?.piso?.beca?.nombre_beca || 'N/A' }}
-            </td>
-            <td class="hidden md:table-cell px-4 py-2 border-b border-gray-200 text-center">
-              {{ estudiante.cuarto?.torre?.piso?.numero_piso || 'N/A' }}
-            </td>
-            <td class="hidden md:table-cell px-4 py-2 border-b border-gray-200 text-center">
-              {{ estudiante.cuarto?.torre?.nombre_torre || 'N/A' }}
-            </td>
-            <td class="hidden md:table-cell px-4 py-2 border-b border-gray-200 text-center">
-              {{ estudiante.cuarto?.numero_cuarto || 'N/A' }}
-            </td>
+            <td class="px-4 py-2 border-b border-gray-200 text-center">{{ estudiante.genero }}</td>
             <td class="px-4 py-2 border-b border-gray-200 text-center">
               <button @click="mostrarEstudiante(estudiante)" class="text-blue-500 hover:underline mr-2">Mostrar</button>
               <button @click="abrirFormulario(estudiante)" class="text-blue-500 hover:underline mr-2">Editar</button>
@@ -127,15 +115,25 @@ const toast = useToast();
 
 const fetchEstudiantes = async () => {
   try {
-    estudiantes.value = await $fetch(`${config.public.backend_url}/estudiantes`, {
+    const data = await $fetch(`${config.public.backend_url}/estudiantes`, {
       headers: {
         'Authorization': token.value
       },
     });
+
+    // Calcular el género para cada estudiante
+    estudiantes.value = data.map(estudiante => {
+      const ultimoDigito = parseInt(estudiante.ci.slice(-1), 10);
+      estudiante.genero = ultimoDigito % 2 === 0 ? 'Masculino' : 'Femenino';
+      return estudiante;
+    });
+
     console.log('Datos de estudiantes:', estudiantes.value);
   } catch (error) {
     console.error('Error al obtener los estudiantes:', error);
-    toast.error('Error al obtener los estudiantes');
+    const errorMessage = error.response?._data?.message || 'Error al obtener los estudiantes';
+    const errorCode = error.response?.status || 500;
+    toast.error(`Error ${errorCode}: ${errorMessage}`);
   }
 };
 
@@ -181,7 +179,7 @@ const handleEstudianteCreado = () => {
 const mostrarEstudiante = (estudiante) => {
   estudianteSeleccionado.value = estudiante;
   isEditing.value = false;
-  isShowing.value = false;
+  isShowing.value = true;
   showModal.value = true;
 };
 
